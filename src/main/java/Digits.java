28e45ff50e4a4cf762ce9a120e7e5a243a93d169
JonathanL <142960970+5ammich@users.noreply.github.com>
2024-04-15 21:51:33 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,13 +6,29 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{ 
+		while (num > 9){
+			digitList.add(num%10);
+			num = num /10;
+		}
+	    	digitList.add(num);
+		ArrayList<Integer> fixer = new ArrayList <Integer>();
+		int k = digitList.size()-1;
+		for (int i = 0; i < digitList.size(); i++){
+			fixer.add(digitList.get(k));
+			k--;
+		}
+		digitList = fixer;
 	}
 
 	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
+	{ 
+		for (int j = 0; j < digitList.size()-1; j++){
+			if (digitList.get(j) > digitList.get(j+1){
+				return false;
+			}
+		}
+		return true;
 
 	}
 	
